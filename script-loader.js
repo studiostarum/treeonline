21004823db8e2a34847eddd6d6255ac150a91fe0
studiostarum <dev@starum.studio>
2024-09-17 15:40:58 +0200

    create 1 file and update 2 files

--- script-loader.js
+++ script-loader.js
@@ -1,12 +1,12 @@
 (function() {
   const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
   const mainScriptUrl = isLocalhost
-    ? 'http://localhost:9000/src/main.js'
-    : 'https://cdn.jsdelivr.net/gh/studiostarum/treeonline@main/dist/main.js'; // Updated to use dist folder
+    ? 'http://localhost:9000/main.js'
+    : 'https://studiostarum.github.io/treeonline/main.js';
 
   const scripts = document.getElementsByTagName('script');
   for (let script of scripts) {
-    if (script.src.includes('main.js')) {
+    if (script.src.includes('MAIN_SCRIPT_URL')) {
       script.src = mainScriptUrl;
       break;
     }
