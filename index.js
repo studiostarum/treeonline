9713c2e232d7e5ed22673e1c459525ffb9e9cb64
studiostarum <dev@starum.studio>
2024-09-17 12:56:01 +0200

    create 8 files and move 4 files

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import all JavaScript files from the js directory
+import './src/js/navbar.js';
+import './src/js/workflowEyebrow.js';
+
+// You can add any additional initialization code here if needed
+console.log('All JS modules loaded');
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        host: '0.0.0.0',
+        allowedHosts: 'all',
+        hot: true,
+    },
+};
