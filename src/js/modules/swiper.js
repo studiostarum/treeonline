18cbcf05df92658996893722314cc3103b063029
Studio Starum <dev@starum.studio>
2025-01-31 11:26:44 +0100

    refactor: Remove Webflow-specific classes from Swiper configuration

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,36 +1,28 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, A11y } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 
 // Import our custom Swiper styles
 import '../../css/modules/swiper.css';
 
 export const initSwiper = () => {
   // Initialize both sliders
-  const aboutSlider1 = new Swiper('.about-us_slider .slider1', {
-    modules: [Navigation, Pagination, A11y],
+  const aboutSlider1 = new Swiper('.slider1', {
+    modules: [Navigation],
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 0,
-    speed: 800,
-    
-    // Navigation arrows
     navigation: {
-      nextEl: '.about-us_slider .gallery20_arrow.swiper-button-next',
-      prevEl: '.about-us_slider .gallery20_arrow.swiper-button-prev',
+      nextEl: '.gallery20_arrow.swiper-button-next',
+      prevEl: '.gallery20_arrow.swiper-button-prev',
     }
   });
 
-  const aboutSlider2 = new Swiper('.about-us_slider .slider2', {
-    modules: [Navigation, Pagination, A11y],
+  const aboutSlider2 = new Swiper('.slider2', {
+    modules: [Navigation],
     loop: true,
     slidesPerView: 1,
-    spaceBetween: 0,
-    speed: 800,
-    
-    // Navigation arrows
     navigation: {
-      nextEl: '.about-us_slider .gallery20_arrow.swiper-button-next',
-      prevEl: '.about-us_slider .gallery20_arrow.swiper-button-prev',
+      nextEl: '.gallery20_arrow.swiper-button-next',
+      prevEl: '.gallery20_arrow.swiper-button-prev',
     }
   });
 
@@ -38,8 +30,8 @@ export const initSwiper = () => {
   console.log('Swiper initialized:', {
     slider1: aboutSlider1,
     slider2: aboutSlider2,
-    slider1Elements: document.querySelectorAll('.about-us_slider .slider1').length,
-    slider2Elements: document.querySelectorAll('.about-us_slider .slider2').length,
+    slider1Elements: document.querySelectorAll('.about-us_slider .slider1.w-dyn-list').length,
+    slider2Elements: document.querySelectorAll('.about-us_slider .slider2.w-dyn-list').length,
     navigationNext: document.querySelectorAll('.about-us_slider .gallery20_arrow.swiper-button-next').length,
     navigationPrev: document.querySelectorAll('.about-us_slider .gallery20_arrow.swiper-button-prev').length
   });
